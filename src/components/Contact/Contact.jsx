b1aac914633f9081ea4d688e81a830e30bc59348
wulforr <gsshaurya@gmail.com>
2020-12-01 11:53:02 +0530

    fixed contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './contact.css'
+import Email from '../../assets/EmailIcon.svg'
 
 export default function Contact() {
   return (
     <section className="contact">
-      <div className="contact-heading">
+      {/* <div className="contact-heading">
         Start a Project
       </div>
       <div className="contact-text">
@@ -12,6 +13,22 @@ export default function Contact() {
       </div>
       <div className="contact-btn-wrapper">
         <button className="btn contact-btn">Let's do this!</button>
+      </div> */}
+      <div className="contact-icon">
+        <img src={Email} alt="email icon" />
+      </div>
+      <div className="contact-desc">
+        <div>
+          <div className="contact-heading">
+            Start a Project
+          </div>
+          <div className="contact-text">
+            I am interested to get in touch with you.
+          </div>
+        </div>
+        <div className="contact-btn-wrapper">
+          <button className="btn contact-btn">Let's do this!</button>
+        </div>
       </div>
     </section>
   )
