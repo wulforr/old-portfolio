fb4342e54bca5e2a593006bc4a417f8b2250b197
wulforr <gsshaurya@gmail.com>
2020-12-17 10:56:38 +0530

    added netlify form

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -9,21 +9,28 @@ export default function ContactPage() {
         <img src={ContactIllustration} alt="contact illustration" />
       </div>
       <div className="contact-wrapper">
+      <form 
+       name="contact" 
+       method="POST" 
+       data-netlify="true" 
+      >
+             <input type="hidden" name="form-name" value="contact" />
         <div className="form-row">
           <label className="input-label">Name</label>
-          <input className="form-input" type="text" />
+          <input className="form-input" type="text" name="name" />
         </div>
         <div className="form-row">
           <label className="input-label">Email</label>
-          <input className="form-input" type="email" />
+          <input className="form-input" type="email" name="email" />
         </div>
         <div className="form-row">
           <label className="input-label">Message</label>
-          <textarea className="form-textarea"></textarea>
+          <textarea className="form-textarea" name="message"></textarea>
         </div>
         <div className="form-row form-btn-row">
-          <button className="btn contact-form-btn">Send Message</button>
+          <button className="btn contact-form-btn" type="submit">Send Message</button>
         </div>
+        </form>
       </div>
     </section>
   )
